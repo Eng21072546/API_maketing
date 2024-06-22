452c81d523b01b747d36872a02c258ffdaf4f9f6
Eng21072546 <thanaphat.boo@student.mahidol.edu>
2024-06-22 13:33:56 +0700

    New gen product id in range 10000-99999

--- useCase/productUsecase.go
+++ useCase/productUsecase.go
@@ -9,10 +9,10 @@ import (
 
 type ProductUseCase interface {
 	CreateProduct(product *entity.Product) (*entity.Product, error)
-	GetProduct(id string) (*entity.Product, error)
+	GetProduct(id int) (*entity.Product, error)
 	GetAllProduct() (*[]entity.Product, error)
-	UpdateProduct(id string, productUpdate *entity.ProductUpdate) (*mongo.UpdateResult, error)
-	DeleteProduct(id string) (*mongo.DeleteResult, error)
+	UpdateProduct(id int, productUpdate *entity.ProductUpdate) (*mongo.UpdateResult, error)
+	DeleteProduct(id int) (*mongo.DeleteResult, error)
 }
 
 type ProductUseCaseImpl struct {
@@ -25,12 +25,13 @@ func NewProductUseCase(repo ProductRepository) ProductUseCase {
 
 func (p *ProductUseCaseImpl) CreateProduct(product *entity.Product) (*entity.Product, error) {
 	rand.Seed(time.Now().UnixNano()) // random id product
-	product.ID = rand.Intn(100000)
+	randomNumber := 10000 + rand.Intn(90001)
+	product.ID = randomNumber
 	_, err := p.repo.InsertProduct(product)
 	return product, err
 }
 
-func (p *ProductUseCaseImpl) GetProduct(id string) (*entity.Product, error) {
+func (p *ProductUseCaseImpl) GetProduct(id int) (*entity.Product, error) {
 	return p.repo.FindProductById(id)
 }
 
@@ -38,10 +39,10 @@ func (p *ProductUseCaseImpl) GetAllProduct() (*[]entity.Product, error) {
 	return p.repo.FindAllProducts()
 }
 
-func (p *ProductUseCaseImpl) UpdateProduct(id string, productUpdate *entity.ProductUpdate) (*mongo.UpdateResult, error) {
+func (p *ProductUseCaseImpl) UpdateProduct(id int, productUpdate *entity.ProductUpdate) (*mongo.UpdateResult, error) {
 	return p.repo.UpdateProduct(id, productUpdate)
 }
 
-func (p *ProductUseCaseImpl) DeleteProduct(id string) (*mongo.DeleteResult, error) {
+func (p *ProductUseCaseImpl) DeleteProduct(id int) (*mongo.DeleteResult, error) {
 	return p.repo.DeleteProductById(id)
 }
