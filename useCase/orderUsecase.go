03bc27dbbb02e158f324a70450e875cc0a89307a
Eng21072546 <thanaphat.boo@student.mahidol.edu>
2024-06-22 13:52:39 +0700

    calculateorder price function

--- useCase/orderUsecase.go
+++ useCase/orderUsecase.go
@@ -2,20 +2,58 @@ package useCase
 
 import (
 	"github.com/Eng21072546/API_maketing/entity"
+	"github.com/google/uuid"
+	"strconv"
 )
 
 type OrderUseCase interface {
-	CreateOrder(order entity.Order) (entity.Order, []string)
+	CalculateOrderPrice(order entity.Order) (entity.Order, []string)
 }
 
 type OrderUseCaseImpl struct {
-	repo OrderRepository
+	orderRepo   OrderRepository
+	productRepo ProductRepository
 }
 
-func NewOrderUseCase(repo OrderRepository) OrderUseCase {
-	return &OrderUseCaseImpl{repo: repo}
+func NewOrderUseCase(orderRepo OrderRepository, productRepo ProductRepository) OrderUseCase {
+	return &OrderUseCaseImpl{orderRepo: orderRepo, productRepo: productRepo}
 }
 
-func (o OrderUseCaseImpl) CreateOrder(order entity.Order) (entity.Order, []string) {
-	return o.repo.SaveOrder(order)
+func (o OrderUseCaseImpl) CalculateOrderPrice(order entity.Order) (entity.Order, []string) {
+	var errList []string
+	err := entity.CheckAddress(order)
+	if err != nil {
+		errList = append(errList, err.Error())
+	}
+	for _, productOrder := range order.ProductList {
+		_, err = o.productRepo.FindProductById(productOrder.ProductID)
+		if err != nil {
+			errList = append(errList, err.Error())
+		}
+	}
+	if len(errList) > 0 {
+		return order, errList
+	}
+	order.ID = uuid.New().String()
+	order.TotalPrice = o.CalculatePrice(order)
+	_, err = o.orderRepo.InsertOrder(order)
+
+	if err != nil {
+		errList = append(errList, err.Error())
+	}
+	return order, errList
+}
+
+func (o OrderUseCaseImpl) CalculatePrice(order entity.Order) float64 {
+	var totalPrice float64
+	var bill []string
+	logisticPrice, _ := entity.LogisticCost(order)
+	totalPrice += logisticPrice
+	for _, productOrder := range order.ProductList {
+		product, _ := o.productRepo.FindProductById(productOrder.ProductID)
+		productPrice := product.Price * float64(productOrder.Quantity)
+		bill = append(bill, product.Name, " ", strconv.FormatFloat(product.Price, 'f', 2, 64), "  ", string(productOrder.Quantity), " ", strconv.FormatFloat(productPrice, 'f', 2, 64))
+		totalPrice += productPrice
+	}
+	return totalPrice
 }
